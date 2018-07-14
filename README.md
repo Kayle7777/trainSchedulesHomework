088e3c7238ff1795674e345cfffaf42be70bff79
Kayle7777 <jessewebb2@gmail.com>
2018-07-14 17:18:47 -0500

    Initial files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Train Schedules!
+
+This is my 7th week homework!! :)
+
+## Authors
+
+* **Jesse Webb** - *All JS, html, CSS* - [Kayle7777](https://github.com/kayle7777)
---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyBZm22gCHksxDr7QAGpiykmTNx6jaAtK3Q",
+  authDomain: "testproject-3924d.firebaseapp.com",
+  databaseURL: "https://testproject-3924d.firebaseio.com",
+  projectId: "testproject-3924d",
+  storageBucket: "testproject-3924d.appspot.com",
+  messagingSenderId: "338557115530"
+};
+firebase.initializeApp(firebaseConfig);
