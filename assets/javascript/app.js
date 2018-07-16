959442ab6c6d7874a4438fcb23ac6064c8e4b467
Kayle7777 <jessewebb2@gmail.com>
2018-07-15 19:31:05 -0500

    i'm go crazy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,10 @@ firebase.initializeApp(firebaseConfig);
 
 let database = firebase.database();
 
+$("#randomTrain").click((e) => {
+  e.preventDefault()
+  dbSet();
+})
 
 let interval = setInterval(function() {
   database.ref('trainSchedule').once("value", (ss) => {
@@ -17,18 +21,17 @@ let interval = setInterval(function() {
       if ('name' in x.val()) {
         database.ref("trainSchedule/"+x.key+"/minutesAway").set(moment(x.val().nextArrivalFormatted, 'HH:mm:ss').diff(moment(), 'm') + 1)
       } else {
-        database.ref('trainSchedule').child(x.key).set(null);
+        database.ref('trainSchedule/'+x.key).set(null);
       }
     })
   })
-}, 5000)
+}, 1000)
 
 database.ref('trainSchedule').on('value', (ss) => {
   $("#tableBody").empty();
   ss.forEach((x) => {
     if (moment(x.val().nextArrivalFormatted, "HH:mm:ss").isBefore(moment())) {
-      database.ref('trainSchedule').child(x.key).set(null);
-      dbSet();
+      database.ref('trainSchedule').child(x.key).set(null) && dbSet();
     }
     $("#tableBody").prepend(`
       <tr>
@@ -42,18 +45,17 @@ database.ref('trainSchedule').on('value', (ss) => {
   })
 })
 
-
 class Train {
   constructor(names = trainNamer(), m = moment(), trainTimesArr = [], timesPerDay, trainFrequency, nextArrival, minutesAway, nextArrivalFormatted) {
     while (true) {
       trainFrequency = (Math.floor(Math.random() * 36) + 1) * 5;
-      if ((19 * 60)%trainFrequency === 0) {
-        timesPerDay = (19 * 60)/trainFrequency;
+      if ((24 * 60)%trainFrequency === 0) {
+        timesPerDay = (24 * 60)/trainFrequency;
         break;
       }
     }
     for (var i = 0; i <= timesPerDay; i++) {
-      trainTimesArr.push(moment('05:00', 'HH:mm').minutes(trainFrequency * i))
+      trainTimesArr.push(moment('00:00', 'HH:mm').minutes(trainFrequency * i))
     }
     trainTimesArr.map((x, i) => {
       if (x.isBefore(m)) {
@@ -67,24 +69,24 @@ class Train {
 }
 function dbSet() {
   mdb = database.ref('trainSchedule');
-  train = new Train;
+  nt = new Train;
   obj = {
-    name: train.names.name,
-    destination: train.names.destination,
-    frequency: train.trainFrequency,
-    nextArrivalFormatted: train.nextArrivalFormatted,
-    minutesAway: train.minutesAway + 1
+    name: nt.names.name,
+    destination: nt.names.destination,
+    frequency: nt.trainFrequency,
+    nextArrivalFormatted: nt.nextArrivalFormatted,
+    minutesAway: nt.minutesAway + 1
   }
   mdb.push(obj)
 
 }
 
 function trainNamer() {
-  let trainPossibilities1 = ["Atlanta", "Polar", "Falken", "JoCo", "Crazy", "Oregon", "Horse-Drawn", "California", "Trainbot 2000"];
-  let trainPossibilities2 = ["Express", "Caravan", "Bus", "Train", "Carriage", "Mag-Lev"];
+  let trainPossibilities1 = ["Atlanta", "Chicken", "Magic", "Polar", "Falken", "JoCo", "Crazy", "Oregon", "Horse-Drawn", "California", "Trainbot 2000"];
+  let trainPossibilities2 = ["Express", "Caravan", "Bus", "Schoolbus", "Train", "Carriage", "Mag-Lev"];
   let t1 = trainPossibilities1[Math.floor(Math.random() * trainPossibilities1.length)], t2 = trainPossibilities2[Math.floor(Math.random() * trainPossibilities2.length)];
-  let destinationPossibilities1 = ["Old", "The", "Big", "Greasy", "New", "West", "North", "South", "East"];
-  let destinationPossibilities2 = ["Jersey", "York", "Amsterdam", "Hell", "Austin", "Dog Island", "Atlantis", "Orion"];
+  let destinationPossibilities1 = ["Old", "The", "Big", "Greasy", "New", "West", "North", "South", "East", "Prison", "Deep Space Habitat"];
+  let destinationPossibilities2 = ["Jersey", "York", "Amsterdam", "Mexico", "Canada", "Zealand", "R'lyeh", "Zion", "Hell", "Austin", "Dog Island", "Atlantis", "Orion"];
   let d1 = destinationPossibilities1[Math.floor(Math.random() * destinationPossibilities1.length)], d2 = destinationPossibilities2[Math.floor(Math.random() * destinationPossibilities2.length)];
   return {name:`${t1} ${t2}`, destination:`${d1} ${d2}`}
 }
